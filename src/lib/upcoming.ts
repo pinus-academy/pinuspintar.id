536877f882b5b499998235717b819f28d06e20ce
Berland Vardy <102161603+bevapr@users.noreply.github.com>
2025-10-17 10:55:35 +0700

    delete dummy

--- src/lib/upcoming.ts
+++ src/lib/upcoming.ts
@@ -1,23 +1,23 @@
 import { upcomingProps } from "./upcomingTypes"
 
 export const upcoming: upcomingProps[] = [
-  {
-    "id": 1,
-    "title": "AI & Machine Learning Bootcamp",
-    "type": "webinar",
-    "date": "10 May 2025",
-    "time": "09:00 WIB",
-    "description": "Pelajari dasar hingga lanjutan AI dan Machine Learning menggunakan Python, TensorFlow, dan scikit-learn dengan mentor profesional.",
-    "image": "/js.png"
-  },
-    {
-    "id": 2,
-    "title": "AI & Machine Learning Bootcamp",
-    "type": "bootcamp",
-    "date": "10 May 2025",
-    "time": "09:00 WIB",
-    "description": "Pelajari dasar hingga lanjutan AI dan Machine Learning menggunakan Python, TensorFlow, dan scikit-learn dengan mentor profesional.",
-    "image": "/js.png"
-  }
+  // {
+  //   "id": 1,
+  //   "title": "AI & Machine Learning Bootcamp",
+  //   "type": "webinar",
+  //   "date": "10 May 2025",
+  //   "time": "09:00 WIB",
+  //   "description": "Pelajari dasar hingga lanjutan AI dan Machine Learning menggunakan Python, TensorFlow, dan scikit-learn dengan mentor profesional.",
+  //   "image": "/js.png"
+  // },
+  //   {
+  //   "id": 2,
+  //   "title": "AI & Machine Learning Bootcamp",
+  //   "type": "bootcamp",
+  //   "date": "10 May 2025",
+  //   "time": "09:00 WIB",
+  //   "description": "Pelajari dasar hingga lanjutan AI dan Machine Learning menggunakan Python, TensorFlow, dan scikit-learn dengan mentor profesional.",
+  //   "image": "/js.png"
+  // }
 ]
 
