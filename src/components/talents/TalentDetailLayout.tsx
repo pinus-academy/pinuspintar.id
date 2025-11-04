e16f0acec764ce830d90709a314ab6b29f3e9602
rizqyrizal <ansoff836@gmail.com>
2025-11-04 12:06:24 +0700

    Enhance TalentPage filtering by adding search functionality and improve layout in TalentDetailLayout for better visual consistency.

--- src/components/talents/TalentDetailLayout.tsx
+++ src/components/talents/TalentDetailLayout.tsx
@@ -20,8 +20,8 @@ export default function TalentDetailLayout({ talent }: TalentDetailLayoutProps) 
 
     return (
         <section className="max-w-8xl space-y-6 mx-auto p-4">
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 w-full items-start">
-                <div className="md:col-span-1 rounded-lg bg-white p-8 items-center text-center flex flex-col shadow-talent h-full">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 w-full items-stretch">
+                <div className="md:col-span-1 rounded-lg bg-white p-8 items-center text-center flex flex-col shadow-sm h-full">
                     <div className="text-center mt-6">
                         <Image
                             src={photo}
@@ -45,17 +45,16 @@ export default function TalentDetailLayout({ talent }: TalentDetailLayoutProps) 
                     </BadgeTalent>
                 </div>
 
-                <div className="md:col-span-3 flex flex-col gap-6 mb-5">
-
-                    <div className="rounded-lg p-6 bg-white shadow-talent min-h-32 md:min-h-40 flex flex-col">
+                <div className="md:col-span-3 flex flex-col gap-6 mb-5 h-full">
+                    <div className="rounded-lg p-6 bg-white shadow-sm min-h-32 md:min-h-40 flex flex-col h-full">
                         <h3 className="font-semibold text-gray-800 mb-2">Ringkasan Deskripsi</h3>
                         <p className="text-sm text-gray-600 break-words">
                             {description || "No detailed description available."}
                         </p>
                     </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                        <div className=" h-full min-h-fit rounded-lg p-6 bg-white shadow-talent">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 h-full">
+                        <div className="h-full min-h-fit rounded-lg p-6 bg-white shadow-sm flex flex-col">
                             <h3 className="font-semibold text-gray-800 mb-2">Skill Teknis</h3>
                             <ul className="text-sm text-gray-600 list-disc list-inside space-y-1 grid grid-cols-1 sm:grid-cols-1 gap-x-8">
                                 {skillset.map((skill, index) => (
@@ -65,7 +64,7 @@ export default function TalentDetailLayout({ talent }: TalentDetailLayoutProps) 
                                 ))}
                             </ul>
                         </div>
-                        <div className=" h-full min-h-fit rounded-lg p-6 bg-white shadow-talent sm:h-64 md:h-auto flex flex-col">
+                        <div className="h-full min-h-fit rounded-lg p-6 bg-white shadow-sm flex flex-col">
                             <h3 className="font-semibold text-gray-800 mb-2">Pendidikan</h3>
                             <ul className="text-sm text-gray-600 list-disc list-inside space-y-1 grid grid-cols-1 sm:grid-cols-1 gap-x-8">
                                 {education.map((edu, index) => (
@@ -83,46 +82,46 @@ export default function TalentDetailLayout({ talent }: TalentDetailLayoutProps) 
                 <h3 className="font-semibold text-gray-800 mb-2">Pengalaman Kerja</h3>
                 <div className="relative">
 
-                <div className="absolute left-4 sm:left-6 top-0 bottom-0 w-0.5 bg-green-primary-2/35"></div>
-                
-                <ul className="space-y-8">
-                    {experiences.map((experience, index) => (
-                        <li key={index} className="relative">
-                            <div className="flex items-start">
-                                <div className="absolute left-2 sm:left-4 w-4 h-4 rounded-full bg-green-secondary border-4 border-white z-10"></div>
-                                
-                                <div className="ml-8 sm:ml-12 flex-1">
-                                    <div className=" p-4 rounded-lg border border-gray-100">
-                                        <div className="flex flex-col sm:flex-row sm:items-center mb-2">
-                                            <span className="font-bold text-green-primary">{experience.position}</span>
-                                            <span className="hidden sm:inline mx-2 text-emerald-700">—</span>
-                                            <span className="text-green-primary-2">{experience.company} ({experience.year})</span>
-                                        </div>
-                                        <div className="mt-3">
-                                            <div className="flex items-center text-sm text-gray-700 mb-1">
-                                                <i className="fas fa-code text-blue-500 mr-2"></i>
-                                                <span className="font-medium">Tech:</span>
-                                                <span className="ml-2">{experience.tech}</span>
+                    <div className="absolute left-4 sm:left-6 top-0 bottom-0 w-0.5 bg-green-primary-2/35"></div>
+
+                    <ul className="space-y-8">
+                        {experiences.map((experience, index) => (
+                            <li key={index} className="relative">
+                                <div className="flex items-start">
+                                    <div className="absolute left-2 sm:left-4 w-4 h-4 rounded-full bg-green-secondary border-4 border-white z-10"></div>
+
+                                    <div className="ml-8 sm:ml-12 flex-1">
+                                        <div className=" p-4 rounded-lg border border-gray-100">
+                                            <div className="flex flex-col sm:flex-row sm:items-center mb-2">
+                                                <span className="font-bold text-green-primary">{experience.position}</span>
+                                                <span className="hidden sm:inline mx-2 text-emerald-700">—</span>
+                                                <span className="text-green-primary-2">{experience.company} ({experience.year})</span>
                                             </div>
-                                            <div className="text-sm text-gray-700">
-                                                <div className="flex items-start">
-                                                    <i className="fas fa-tasks text-blue-500 mr-2 mt-0.5"></i>
-                                                    <div>
-                                                        <span className="font-medium">Responsibilities:</span>
-                                                        <span className="ml-2">
-                                                            {experience.responsibility}
-                                                        </span>
+                                            <div className="mt-3">
+                                                <div className="flex items-center text-sm text-gray-700 mb-1">
+                                                    <i className="fas fa-code text-blue-500 mr-2"></i>
+                                                    <span className="font-medium">Tech:</span>
+                                                    <span className="ml-2">{experience.tech}</span>
+                                                </div>
+                                                <div className="text-sm text-gray-700">
+                                                    <div className="flex items-start">
+                                                        <i className="fas fa-tasks text-blue-500 mr-2 mt-0.5"></i>
+                                                        <div>
+                                                            <span className="font-medium">Responsibilities:</span>
+                                                            <span className="ml-2">
+                                                                {experience.responsibility}
+                                                            </span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </section>
     );
